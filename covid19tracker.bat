bae773160476ca4e30db45266c10c234d00d4b8b
deeptadeeproy <deeptadeeproy@yahoo.in>
2020-04-09 21:22:31 +0530

    All platforms : Optimizations

--- covid19tracker.bat
+++ covid19tracker.bat
@@ -1,13 +1,7 @@
 ECHO ON
 ECHO This is a batch file to run the main script on windows.
-REM '''
-REM COVID-19 Tracker India is simple and fast program to track all cases of COVID-19 in India and can be run on most
-REM platforms without any hassle.
-REM Please report bugs at https://github.com/deeptadeeproy/COVID-19-Tracker-India
-REM '''
 
 
 python dist\app.py
-
 
 PAUSE
--- dist/app.py
+++ dist/app.py
@@ -1,3 +1,10 @@
 import main
 
+
+print('''
+COVID-19 Tracker India is simple and fast program to track all cases of COVID-19 in India and can be run on most
+platforms without any hassle.
+Please report bugs at https://github.com/deeptadeeproy/COVID-19-Tracker-India\n
+''')
+
 main.user_menu()
